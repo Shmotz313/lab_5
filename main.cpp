88b3691e846ef96904089002f6a8289263e14267
Dmitriy Davydov <shmotz313@gmail.com>
2019-06-02 21:00:40 +0300

    Немного изменил код

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 //Добавлен комментарий
 int main()
 {
-
+        cout << "Введите a: ";
 	int a;
+        cout << "Введите b: ";
 	int b;
 	std::cin >> a;
 	std::cin >> b;	
-	std::cout << a + b;
-	std::cout << a - b;
-	std::cout << a * b;
-	std::cout << a / b;}
+	std::cout <<"Сумма: " <<a + b;
+	std::cout <<"Разность: " <<a - b;
+	std::cout <<"Умножение: " <<a * b;
+	std::cout <<"Деление: " <<a / b;
+
+}
 
